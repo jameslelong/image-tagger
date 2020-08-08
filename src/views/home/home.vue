4083a76109da032d4d0a0c9acdd329ed5be0e72c
James <lelongjames@gmail.com>
2020-08-08 14:09:21 +1000

    Webpage now displays message on smaller screen sizes
    - This is not an website that requires Mobile support due to it's
    purpose, but it should still be accounted for in some regard with a
    message.

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,14 +1,20 @@
 <template>
-  <section id="home">
-    <carousel v-bind:images="images" v-bind:selectedImage="selectedImage" v-on:image-selected="selectImage"></carousel>
-    <div id="primary-area">
-      <div id="editor-column">
-        <editor-canvas v-bind:selectedImage="selectedImage" v-bind:selectedTag="selectedTag"></editor-canvas>
-        <image-upload v-on:image-uploaded="createImage"></image-upload>
+  <main id="container">
+    <section id="home">
+      <carousel v-bind:images="images" v-bind:selectedImage="selectedImage" v-on:image-selected="selectImage"></carousel>
+      <div id="primary-area">
+        <div id="editor-column">
+          <editor-canvas v-bind:selectedImage="selectedImage" v-bind:selectedTag="selectedTag"></editor-canvas>
+          <image-upload v-on:image-uploaded="createImage"></image-upload>
+        </div>
+        <settings-column v-bind:tags="tags"  v-bind:selectedTag="selectedTag" v-bind:images="images" v-bind:selectedImage="selectedImage" v-on:tag-selected="selectTag"></settings-column>
       </div>
-      <settings-column v-bind:tags="tags"  v-bind:selectedTag="selectedTag" v-bind:images="images" v-bind:selectedImage="selectedImage" v-on:tag-selected="selectTag"></settings-column>
-    </div>
-  </section>
+    </section>
+
+    <section id="screen-size-alert">
+      <p>This screen size is not supported</p>
+    </section>
+  </main>
 </template>
 
 <script src="./home.ts"></script>
